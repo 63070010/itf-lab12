52befb11825d5b579085ff0f4f07fcbccce52d1d
63070010 <73679810+63070010@users.noreply.github.com>
2020-11-20 01:03:46 +0700

    Create edit.php

---
+++ edit.php
@@ -0,0 +1,32 @@
+<?php
+    $conn = mysqli_connect('63070054itf.mysql.database.azure.com', 'waveza2@63070054itf', 'Zaza456654', 'ITFlab', 3306);
+    $sql = 'SELECT * FROM guestbook WHERE ID = '.$_GET['ID'].'';
+    $query = mysqli_query($conn, $sql);
+    if(!$query) {
+        header('Location: show.php');
+    }
+    else {
+        $data = mysqli_fetch_assoc($query);
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Comment Form</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <form action = "insert.php" method = "post">
+    <p class="text-muted">Name:<br></p>
+    <input type="text" name = "Name" placeholder="Enter Name"> <br>
+    <p class="text-muted">Comment:<br></p>
+    <textarea rows="10" cols="20" name = "Comment" placeholder="Enter Comment"></textarea><br>  
+    <p class="text-muted">Link:<br></p>
+    <input type="text" name = "Link" placeholder="Enter Link"> <br><br>
+    <button type="submit" class="btn btn-outline-secondary">Submit</button>
+  </form> 
+</body>
+</html>
