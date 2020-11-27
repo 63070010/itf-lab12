59c716914f5c46aaa43860a38f7824ba27f593de
63070010 <73679810+63070010@users.noreply.github.com>
2020-11-27 10:49:00 +0700

    Create delete1.php

---
+++ delete1.php
@@ -0,0 +1,25 @@
+ <?php
+    $conn = mysqli_connect('itf-lab12.mysql.database.azure.com', 'it63070010@itf-lab12', 'PJPvjx84', 'itflab', 3306);
+    $sql = 'DELETE FROM guestbook WHERE ID = '.$_GET['ID'].'';
+    if(mysqli_query($conn, $sql)) {
+        echo "COMPLETED ^ ^";
+    }
+    else {
+        echo "FAILED T T";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Delete</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <br><a href="show1.php" class="btn btn-primary">BACK</a>
+</body>
+</html>
